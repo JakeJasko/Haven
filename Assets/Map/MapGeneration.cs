6c2ab27063c02f11b361adfaa8c566a377eb3157
Scytone <swkatcher@gmail.com>
2014-04-07 15:01:59 -0700

    Created Random Map Generation

--- Assets/Map/MapGeneration.cs
+++ Assets/Map/MapGeneration.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+
 
 public class MapGeneration : MonoBehaviour {
+	string mapText;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		createRandomMap ();
 	}
 	
 	// Update is called once per frame
@@ -14,12 +18,38 @@ public class MapGeneration : MonoBehaviour {
 	}
 
 	void createRandomMap() {
-
-		int x = 30;
-		int y = 30;
-
-		int tileMapSize = 2;
+		string mapFile = "Assets/Map/Map.txt";
 
 
-	}
+
+		int playerMapX = 30;
+		int playerMapY = 30;
+
+		int tileMapSize = 3;
+
+	
+
+		for(int y=0; y < playerMapY; y++){
+			for(int x=0; x< playerMapX; x++){
+				mapText = mapText+Random.Range (0,tileMapSize);
+			}
+			mapText=mapText+"\n";
+		}
+
+		if(System.IO.File.Exists (mapFile)){
+			Debug.Log (mapFile+" already exists. Overwriting");
+			using(TextWriter textWriter = File.CreateText (mapFile)){
+				textWriter.Write (mapText);
+			}
+			Debug.Log (mapFile+" created.");
+			
+		}else{
+			using(TextWriter textWriter = File.CreateText (mapFile)){
+				textWriter.Write (mapText);
+			}
+			Debug.Log (mapFile+" created.");
+		}
+
+
+}
 }
